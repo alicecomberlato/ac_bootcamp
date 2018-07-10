9b67b55be615fb627cdae65cc40df70ed7242031
Alice Comberlato <alice.comberlato@epfl.ch>
2018-07-10 11:33:54 +0200

    Initial commit of bootcamp utilities.

---
+++ ac_bootcamp/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for utilities for bootcamp."""
+
+from .na_utils import *
+from .bioinfo_dicts import *
+
+__author__ = 'Alice Comberlato'
+__email__ = 'alice.comberlato@epfl.ch'
+__version__ = '0.0.1'
